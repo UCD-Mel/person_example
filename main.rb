4556bd4a019db2cd9effad7e4b6db34f3d5839ac
Mel Ó Cinnéide <mel.ocinneide@gmail.com>
2020-09-29 17:16:34 +0100

    Person increment_age puts statements added

--- main.rb
+++ main.rb
@@ -2,5 +2,5 @@
 require_relative 'Person'
 
 john = Person.new('John')
-10.times {john.increment_age}
+20.times {john.increment_age}
 puts john.to_s
--- person.rb
+++ person.rb
@@ -7,6 +7,11 @@ class Person
 
   def increment_age
     @age += 1
+    if @age == 13
+      puts "I'm a teenager"
+    elsif @age == 18
+      puts "I'm an adult"
+    end
   end
 
   def to_s
